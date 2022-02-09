604c0063397e7934f7fdd0b30eadc42b06a652ba
dandeduck <danielmkhlv@gmail.com>
2022-02-09 12:22:39 +0200

    Added https, self signed for now

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { configService } from './config/config.service';
 
+const fs = require('fs');
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync('./cert/key.pem'),
+    cert: fs.readFileSync('./cert/cert.pem'),
+  };
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions
+  });
   app.setGlobalPrefix('v1');
   await app.listen(configService.getPort());
 }
